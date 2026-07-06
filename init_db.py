590090ffce7a758127258be81dc824412fd51797
Shivam Wadje <shivamwadje22@gmail.com>
2026-07-06 02:00:21 -0700

    fixed

--- init_db.py
+++ init_db.py
@@ -4,42 +4,38 @@ from werkzeug.security import generate_password_hash
 
 def init_db():
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    DB_PATH = os.path.join(BASE_DIR, 'database.db')
-    
+    DB_PATH = os.path.join(BASE_DIR, "database.db")
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS visitors (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            visitor_name TEXT NOT NULL,
-            student_name TEXT NOT NULL,
-            room_number TEXT NOT NULL,
-            visit_date TEXT,
-            purpose TEXT NOT NULL,
-            in_time TEXT NOT NULL,
-            out_time TEXT,
-            status TEXT DEFAULT 'Inside'
-        )
-    ''')
-    
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT UNIQUE NOT NULL,
-            password TEXT NOT NULL,
-            role TEXT DEFAULT 'user'
-        )
-    ''')
-    
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS visitors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    visitor_name TEXT NOT NULL,
+    student_name TEXT NOT NULL,
+    room_number TEXT NOT NULL,
+    visit_date TEXT,
+    purpose TEXT NOT NULL,
+    in_time TEXT NOT NULL,
+    out_time TEXT,
+    status TEXT DEFAULT "Inside"
+    )""")
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT DEFAULT "user"
+    )""")
+
     # Default admin user
     cursor.execute("SELECT * FROM users WHERE username = 'admin'")
     if not cursor.fetchone():
-        hashed_pw = generate_password_hash('admin123')
-        cursor.execute("INSERT INTO users (username, password, role) VALUES (?, ?, ?)", ('admin', hashed_pw, 'admin'))
-    
+        hashed_pw = generate_password_hash("admin123")
+        cursor.execute("INSERT INTO users (username, password, role) VALUES (?, ?, ?)", ("admin", hashed_pw, "admin"))
+
     conn.commit()
     conn.close()
-    print("Database initialized!")
-    if __name__== '__main__':
-        init_db()
+    print("Database Initialized!")
+
+if __name__ == "__main__":
+    init_db()
